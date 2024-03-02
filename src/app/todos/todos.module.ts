57317aec3f02b24de3cde76d4f23d1df002a3551
ken778 <kennethtumi18@gmail.com>
2024-03-02 22:52:48 +0200

    first commit

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core'
+import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from 'src/app/todos/components/todos/todos.component';
+
+const routes:Routes=[
+    {
+        path:'',
+        component:TodosComponent
+    }
+] 
+@NgModule({
+    declarations: [TodosComponent],
+    imports:[RouterModule.forChild(routes)],
+})
+export class TodosModule{}
