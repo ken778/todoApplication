9b1092c289259b9cf39a3dc3f4577a59b2d018e6
ken778 <kennethtumi18@gmail.com>
2024-03-03 10:22:52 +0200

    add todo service

--- src/app/todos/components/header/header.component.ts
+++ src/app/todos/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { TodosService } from "../../services/todos.service";
 
 @Component({
     selector: "app-todos-header",
@@ -9,6 +10,10 @@ import { Component } from "@angular/core";
 export class HeaderComponent {
 
     text: string = ""
+
+    constructor(private todoService: TodosService) { 
+       
+    }
     changeText(event: Event): void {
         const target = event.target;
 
@@ -17,6 +22,8 @@ export class HeaderComponent {
         }
     }
     addTodo(): void {
-        console.log('addTodo', this.text)
+        this.todoService.addTodo(this.text)
+        this.text = "";
+       
     }
 }
