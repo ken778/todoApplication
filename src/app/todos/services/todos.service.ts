1662241080d6e4c9821f9470a00f92e3a0572332
ken778 <kennethtumi18@gmail.com>
2024-03-03 13:15:13 +0200

    reder todo cpmponent created

--- src/app/todos/services/todos.service.ts
+++ src/app/todos/services/todos.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { TodoInterface } from '../types/todo.interface';
+import { FilterEnum } from "../types/filter.enum";
 
 @Injectable()
 export class TodosService {
     todos$ = new BehaviorSubject<TodoInterface[]>([]);
+    filter$ = new BehaviorSubject<FilterEnum>(FilterEnum.all);
 
     addTodo(text: string):void{
         const newTodo: TodoInterface = {
@@ -15,4 +17,6 @@ export class TodosService {
         const updatedTodos = [...this.todos$.getValue(), newTodo]
         this.todos$.next(updatedTodos)
     }
+
+    
 }
